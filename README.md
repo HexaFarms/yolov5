f53716213d8f109de7eab7f389e58e25775a3ddc
ccomkhj <ccomkhj@gmail.com>
2022-03-15 15:27:58 +0100

    data_filter update

--- README.md
+++ README.md
@@ -3,57 +3,9 @@
 ## What is different here?
 It's speciallized for drone's detection model in indoor farm.
 
-
-
-
-<div align="center">
-
-<br>
-<div>
-   <a href="https://github.com/ultralytics/yolov5/actions"><img src="https://github.com/ultralytics/yolov5/workflows/CI%20CPU%20testing/badge.svg" alt="CI CPU testing"></a>
-   <a href="https://zenodo.org/badge/latestdoi/264818686"><img src="https://zenodo.org/badge/264818686.svg" alt="YOLOv5 Citation"></a>
-   <a href="https://hub.docker.com/r/ultralytics/yolov5"><img src="https://img.shields.io/docker/pulls/ultralytics/yolov5?logo=docker" alt="Docker Pulls"></a>
-   <br>
-   <a href="https://colab.research.google.com/github/ultralytics/yolov5/blob/master/tutorial.ipynb"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"></a>
-   <a href="https://www.kaggle.com/ultralytics/yolov5"><img src="https://kaggle.com/static/images/open-in-kaggle.svg" alt="Open In Kaggle"></a>
-   <a href="https://join.slack.com/t/ultralytics/shared_invite/zt-w29ei8bp-jczz7QYUmDtgo6r6KcMIAg"><img src="https://img.shields.io/badge/Slack-Join_Forum-blue.svg?logo=slack" alt="Join Forum"></a>
-</div>
-
-<br>
-<p>
-YOLOv5 🚀 is a family of object detection architectures and models pretrained on the COCO dataset, and represents <a href="https://ultralytics.com">Ultralytics</a>
- open-source research into future vision AI methods, incorporating lessons learned and best practices evolved over thousands of hours of research and development.
-</p>
-
-<div align="center">
-   <a href="https://github.com/ultralytics">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-github.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://www.linkedin.com/company/ultralytics">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-linkedin.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://twitter.com/ultralytics">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-twitter.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://www.producthunt.com/@glenn_jocher">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-producthunt.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://youtube.com/ultralytics">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-youtube.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://www.facebook.com/ultralytics">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-facebook.png" width="2%"/>
-   </a>
-   <img width="2%" />
-   <a href="https://www.instagram.com/ultralytics/">
-   <img src="https://github.com/ultralytics/yolov5/releases/download/v1.0/logo-social-instagram.png" width="2%"/>
-   </a>
-</div>
+## Previous dataset can be re-used considering which classes you are interested.
+`data_filter.py` \
+ex) only person, labtop, cell phone, plant from coco dataset can be used for your custom dataset
 
 <!--
 <a align="center" href="https://ultralytics.com/yolov5" target="_blank">
--- data_filter.py
+++ data_filter.py
@@ -6,20 +6,27 @@ from loguru import logger
 
 """ Filter YOLO Dataset to use only relevant class """
 
+INPUT_LABEL_DIR = "/media/hexaburbach/onetb/yolo_data/coco/labels/train2017"
+OUTPUT_LABEL_DIR = "/media/hexaburbach/onetb/yolo_data/coco/labels/filtered_train"
+INPUT_IMG_DIR = "/media/hexaburbach/onetb/yolo_data/coco/images/train2017"
+OUTPUT_IMG_DIR = "/media/hexaburbach/onetb/yolo_data/coco/images/filtered_train"
+INTEREST_CLASS = [0, 58, 63, 67]  # Interested class in the original dataset
+MAPPING_CLASS = {0: 1, 58: 0, 63: 2, 67: 3}  # {index of original dataset : index of new dataset}
+
 if __name__ == "__main__":
 
     logger.info("Start Data Filtering.")
 
-    path_label = Path("/media/hexaburbach/onetb/yolo_data/coco/labels/train2017")
+    path_label = Path(INPUT_LABEL_DIR)
     labels = list(path_label.glob("*.txt"))
-    save_loc_label = Path("/media/hexaburbach/onetb/yolo_data/coco/labels/filtered_train")
+    save_loc_label = Path(OUTPUT_LABEL_DIR)
 
-    path_img = Path("/media/hexaburbach/onetb/yolo_data/coco/images").joinpath(path_label.stem)
-    save_loc_img = Path("/media/hexaburbach/onetb/yolo_data/coco/images/").joinpath(save_loc_label.stem)
+    path_img = Path(INPUT_IMG_DIR)
+    save_loc_img = Path(OUTPUT_IMG_DIR)
     used_files = labels.copy()
 
-    classes = [0, 58, 63, 67]  # person, potted plant, laptop, cell phone 
-    new_class_idx = {0:1, 58:0, 63:2, 67:3} # map to the new labels
+    classes = INTEREST_CLASS  # person, potted plant, laptop, cell phone
+    new_class_idx = MAPPING_CLASS  # map to the new labels
 
     logger.info(f"{len(labels)} images will be processed.")
     """ Leave only relevant classes"""
@@ -27,28 +34,29 @@ if __name__ == "__main__":
 
         text = np.genfromtxt(file, delimiter=" ")
 
-        if len(text.shape)==1: # one object only, then keep the format.
-            text = np.expand_dims(text,axis=0)
+        if len(text.shape) == 1:  # one object only, then keep the format.
+            text = np.expand_dims(text, axis=0)
 
-        if not bool( set(text[:,0]) & set(classes) ):
+        if not bool(set(text[:, 0]) & set(classes)):
             used_files.remove(file)
             logger.info(f"{file} has no relevant classes. So, this image will be deleted.")
             continue
 
-        text = np.delete(text, np.argwhere(np.isin(text[:,0], classes, invert=True)).ravel(), axis=0)
-        text[:,0] = np.vectorize(new_class_idx.get)(text[:,0]) # change to the new lebeling number.
+        text = np.delete(text, np.argwhere(np.isin(text[:, 0], classes, invert=True)).ravel(), axis=0)
+        text[:, 0] = np.vectorize(new_class_idx.get)(text[:, 0])  # change to the new lebeling number.
 
         """ Convert to pandas to convert the first column to integer"""
         df = pd.DataFrame(data=text).astype('float16')
         df[0] = df[0].astype('int8')
-        df.to_csv(os.path.join(save_loc_label,file.name), index=False, header=False)
+        df.to_csv(os.path.join(save_loc_label, file.name), index=False, header=False, sep=' ')
 
-    logger.info(f"Now relevant images are moved.")    
+    logger.info(f"Now relevant images are moved.")
     for used_file in used_files:
         try:
-            img_name = path_img.joinpath(used_file.stem).with_suffix('.jpg') # if the input format is different, should be changed.
+            # if the input format is different, should be changed.
+            img_name = path_img.joinpath(used_file.stem).with_suffix('.jpg')
             img_name.rename(os.path.join(save_loc_img, img_name.name))
         except:
             logger.error(f"{used_file} is not found in the image folder.")
 
-    logger.info(f"{len(used_files)} images are successfully processed.")    
+    logger.info(f"{len(used_files)} images are successfully processed.")
